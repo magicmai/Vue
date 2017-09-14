1bbc62a3ae69cffc15a04481139ff2c1aee67cd0
magicmai <maijing_wy@163.com>
2017-09-14 10:13:30 +0800

    add files

--- 18.x Vue.js精讲/js/18.16.js
+++ 18.x Vue.js精讲/js/18.16.js
@@ -1,5 +1,39 @@
-Vue.component('tooltip', {
+var base = {
+	methods: {
+		show: function() {
+			this.visible = true;
+		},
+		hide: function () {
+			this.visible = false;
+		},
+		toggle: function () {
+			this.visible = !this.visible;
+		}
+	},
+	data: function() {
+		return {
+			visible: false,
+		}
+	}
+};
 
+Vue.component('tooltip', {
+	template: `
+		<div>
+			<span @mouseenter="show" @mouseleave="hide">show</span>
+			<div v-if="visible">
+				You can see this!
+			</div>
+		</div>
+	`,
+	mixins: [base],
+
+	// 可以覆盖 mixins 定义的属性
+	data: function () {
+		return {
+			visible: true
+		}
+	},
 });
 
 Vue.component('popup', {
@@ -7,26 +41,16 @@ Vue.component('popup', {
 		<div>
 			<button @click="toggle">Popup</button>
 			<div v-if="visible">
+				<span @click="hide">X</span>
 				<h4>Tilte</h4>
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum quaerat, aut tempora sint cupiditate vitae quam voluptatibus eveniet. Odio molestias reiciendis sequi autem adipisci eum ducimus labore vero nisi doloribus.
 			</div>
 		</div>
 	`,
-	methods: {
-		toggle: function () {
-			this.visible = !this.visible;
-		}
-	},
-	data: function () {
-		return {
-			visible: false,
-		}
-	},
+	mixins: [base],
 });
 
 new Vue({
 	el: '#app',
-	data: {
-
-	}
+	data: {}
 });
--- 18.x Vue.js精讲/js/18.x.js
+++ 18.x Vue.js精讲/js/18.x.js
@@ -24,9 +24,28 @@ Vue.directive('', function (el, binding) {
 	// body...
 });
 
+// mixins
+var base = {
+	methods: {
+		method1: function () {
+			// body...
+		},
+		method2: function () {
+			// body...
+		}
+	},
+	data: function () {
+		return {
+			key1: "value1",
+			key2: "value2",
+			key3: "value3"
+		}
+	}
+};
+
 var app = new Vue({
 	el: '#app',
 	data: {
-		: ,
+		key: value,
 	}
 });
